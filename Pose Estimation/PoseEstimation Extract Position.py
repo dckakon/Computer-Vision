c4f8eb017608828dafb748bc9869cfea552099c6
Debobrata Chakraborty <61970194+Debobrata-Chakraborty-Kakon@users.noreply.github.com>
2021-06-26 16:29:02 +0600

    Extract Position

---
+++ Pose Estimation/PoseEstimation Extract Position.py
@@ -0,0 +1,46 @@
+import cv2
+import mediapipe as mp #framework for pose estimation
+import time
+import numpy as np
+
+mpDraw=mp.solutions.drawing_utils
+mpPose=mp.solutions.pose
+pose=mpPose.Pose()
+
+cap=cv2.VideoCapture(0)#"PoseVideos/Penalty01.mp4"
+pTime=0
+while True:
+    success, img=cap.read()
+
+    imgRGB=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    results = pose.process(imgRGB)
+    #print(results.pose_landmarks)
+
+
+    if results.pose_landmarks:
+        mpDraw.draw_landmarks(img,results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+        for id, lm in enumerate(results.pose_landmarks.landmark):
+            h, w, c =img.shape
+            print(id, lm)
+            cx,cy= int(lm.x*w), int(lm.y*h)
+            cv2.circle(img, (cx,cy),3 , (255,0,0),cv2.FILLED)
+    h, w, c = img.shape
+    img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Image", img)
+
+    opImg=np.zeros([h,w,c])
+    opImg.fill(255)
+    mpDraw.draw_landmarks(opImg,results.pose_landmarks,mpPose.POSE_CONNECTIONS,
+                          mpDraw.DrawingSpec((255,0,0),2,2))
+    cv2.imshow("Extract Pose",opImg)
+
+
+    cTime=time.time()
+    fps=1/(cTime-pTime)
+    pTime=cTime
+
+    #cv2.putText(img,str(int(fps)),(70,50),cv2.FONT_HERSHEY_PLAIN,3,(255,0,0),3)
+
+
+    cv2.waitKey(1)
+
